9ceca0c3546bf79585b77106faaa07e89f2d2858
ricardcutzh <ricardcutzh@gmail.com>
2019-05-03 14:16:45 -0600

    salidas para ver en los logs@

--- nodejs_code/Database/db.js
+++ nodejs_code/Database/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : '192.168.0.2', //ESTE ES EL NOMBRE DE CONTENEDOR EN EL AMBIENTE DE DESARROLLO
+    host     : 'dbBanca', //ESTE ES EL NOMBRE DE CONTENEDOR EN EL AMBIENTE DE DESARROLLO
     user     : 'root',
     password : 'admin123',
     database : 'banca'
