7ccfd8bee0f4c81266951d9ad501d16c320f38ff
ricardcutzh <ricardcutzh@gmail.com>
2019-05-03 15:31:30 -0600

    agregando cors al server

--- nodejs_code/app.js
+++ nodejs_code/app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var cons = require('./Database/db');
 
+while(true)
+{
+    try
+    {
+        cons.connect();
+        break;
+    }
+    catch(error)
+    {
+
+    }
+}
 
 //ESTE ES UN COMENTARIO
 var app = express();
@@ -9,7 +21,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
+
 //UTILIZANO EL BODY PARSER
 var bodyparser = require('body-parser');
 
