a199ecd4b68e9d8ad78a354fadca184c2590965c
ricardcutzh <ricardcutzh@gmail.com>
2019-05-03 22:25:53 -0600

    agregando hostname en docker

--- nodejs_code/Database/db.js
+++ nodejs_code/Database/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'dbBanca', //ESTE ES EL NOMBRE DE CONTENEDOR EN EL AMBIENTE DE DESARROLLO
+    host     : 'banco', //ESTE ES EL NOMBRE DE CONTENEDOR EN EL AMBIENTE DE DESARROLLO
     user     : 'root',
     password : 'admin123',
     database : 'banca'
